59273712878917f8a8fa2dc22a2b99878975bb25
Nathan Sadler <nathan@sadlerville.com>
2022-08-03 15:42:34 -0400

    spike js now uses json stringify

--- spike.js
+++ spike.js
@@ -1,25 +1,22 @@
 const fs = require('fs')
+const variables = {}
 
 try {
   const baseDirectory = './src/core/tokens'
   const targetFile = './docs/test_structure.json'
 
-  fs.writeFileSync(targetFile, '{')
-
   fs.readdir(baseDirectory, (_errors, files) => {
     files.filter((fileName) => fileName !== 'dark_mode_tokens.scss').forEach((fileName) => {
       const fileContents = fs.readFileSync(`${baseDirectory}/${fileName}`)
       const matches = fileContents.toString().match(/^\s*--.*?(?=;)/gm)
-      console.log('number of matches for', fileName, matches?.length || 0)
       matches.forEach((match) => {
         const [key, value] = match.split(':')
-        fs.writeFileSync(targetFile, `"${key.trim().slice(5)}": "${value.trim()}",`, {flag: 'a+'})
+        variables[key.trim()] = value.trim()
       })
     })
-    fs.writeFileSync(targetFile, '}', {flag: 'a+'})
+    console.log('now variables is', variables)
+    fs.writeFileSync(targetFile, JSON.stringify(variables))
   })
-
-
 
 } catch(err) {
   console.log(err)
