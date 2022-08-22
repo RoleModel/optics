80b764c4ff3798c6fa9dbe2bb79da4d6c579eaef
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2022-08-22 12:48:18 -0400

    [GHI #59] Badges (#63)
    This PR adds a new component. Badges (a.k.a chips, labels, etc) provide a simple and colorful way to add information to your badge. It also adds a way to create a notification-style button by combining the badge with a button.

---
+++ src/stories/Badge.stories.js
@@ -0,0 +1,78 @@
+import { createBadge } from './Badge/Badge.js';
+import BadgeDocs from './Badge/Badge.mdx';
+
+export default {
+  title: 'Components/Badge',
+  argTypes: {
+    label: { control: 'text' },
+    style: {
+      control: { type: 'select' },
+      options: ['default', 'primary', 'warning', 'danger', 'info', 'notice'],
+    },
+    pill: { control: 'boolean' },
+    inButton: {
+      control: { type: 'select' },
+      options: ['no', 'right', 'left'],
+    },
+  },
+  parameters: {
+    docs: {
+      page: BadgeDocs,
+    },
+  },
+};
+
+const Template = ({ label, ...args }) => {
+  return createBadge({ label, ...args });
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Default',
+  style: 'default',
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'Primary',
+  style: 'primary',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  label: 'Warning',
+  style: 'warning',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  label: 'Danger',
+  style: 'danger',
+};
+
+export const Info = Template.bind({});
+Info.args = {
+  label: 'Info',
+  style: 'info',
+};
+
+export const Notice = Template.bind({});
+Notice.args = {
+  label: 'Notice',
+  style: 'notice',
+};
+
+export const Pill = Template.bind({});
+Pill.args = {
+  label: 'Pill',
+  style: 'default',
+  pill: true
+};
+
+export const NotificationButton = Template.bind({});
+NotificationButton.args = {
+  label: '5',
+  style: 'danger',
+  pill: true,
+  inButton: 'right'
+};
