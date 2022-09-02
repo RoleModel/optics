29f4b7fa023dedb9ad40ab29eb9ad9751ca4bd18
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2022-09-02 17:28:02 -0400

    Add documentation for border tokens and link between shadows and it

---
+++ src/stories/Border/Border.js
@@ -0,0 +1,23 @@
+export const createBorder = ({
+  position = 'all',
+  inset = false,
+}) => {
+  const borderElement = document.createElement('div')
+  borderElement.innerText = position
+
+  borderElement.style.padding = 'var(--rm-space-medium)'
+
+  let shadowStyle = `var(--rm-border-${position})`
+
+  if (inset) {
+    shadowStyle = `inset ${shadowStyle}`
+  }
+
+  if (position !== 'x' && position !== 'y') {
+    shadowStyle += ' var(--rm-border-color)'
+  }
+
+  borderElement.style.boxShadow = shadowStyle;
+
+  return borderElement;
+};
