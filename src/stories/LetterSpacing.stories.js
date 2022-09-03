c9659d7a7cb9c7d8f76a29e82a776574de5aaaa6
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2022-09-03 18:21:27 -0400

    Add letter spacing token documentation

---
+++ src/stories/LetterSpacing.stories.js
@@ -0,0 +1,32 @@
+import { createLetterSpacing } from './LetterSpacing/LetterSpacing.js';
+import LetterSpacingDocs from './LetterSpacing/LetterSpacing.mdx';
+
+export default {
+  title: 'Tokens/Letter Spacing',
+  argTypes: {
+    spacing: {
+      control: { type: 'select' },
+      options: ['label', 'navigation'],
+    },
+  },
+  parameters: {
+    docs: {
+      page: LetterSpacingDocs
+    },
+  },
+};
+
+
+const Template = ({ spacing, ...args }) => {
+  return createLetterSpacing({ spacing, ...args });
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  spacing: 'label',
+};
+
+export const Navigation = Template.bind({});
+Navigation.args = {
+  spacing: 'navigation',
+};
---
+++ src/stories/LetterSpacing/LetterSpacing.js
@@ -0,0 +1,8 @@
+export const createLetterSpacing = ({ spacing = 'label' }) => {
+  const element = document.createElement('div')
+
+  element.innerText = "Spacing"
+  element.style.letterSpacing = `var(--rm-letter-spacing-${spacing})`
+
+  return element;
+};
