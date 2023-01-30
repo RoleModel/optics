ba5a1833dc66faab6634f51927ae2839fce6cc0d
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-01-30 17:54:43 -0500

    Rename rm prefix to op

--- src/stories/LetterSpacing/LetterSpacing.js
+++ src/stories/LetterSpacing/LetterSpacing.js
@@ -2,7 +2,7 @@ export const createLetterSpacing = ({ spacing = 'label' }) => {
   const element = document.createElement('div')
 
   element.innerText = 'Spacing'
-  element.style.letterSpacing = `var(--rm-letter-spacing-${spacing})`
+  element.style.letterSpacing = `var(--op-letter-spacing-${spacing})`
 
   return element
 }
