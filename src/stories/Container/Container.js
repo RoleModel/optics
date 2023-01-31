19bb59fb4c872b72db95f3be57073fbd1a35993a
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2023-01-31 09:18:31 -0500

    [GHI #132] Rename rm prefix to op (#132)
    This PR renames the rm prefix for tokens to op

--- src/stories/Container/Container.js
+++ src/stories/Container/Container.js
@@ -1,10 +1,10 @@
 export const createContainer = ({ size = '', padding = '' }) => {
   const page = document.createElement('div')
-  page.style.backgroundColor = 'var(--rm-color-alerts-warning-base)'
+  page.style.backgroundColor = 'var(--op-color-alerts-warning-base)'
   page.style.height = '30rem'
 
   const container = document.createElement('div')
-  container.style.backgroundColor = 'var(--rm-color-alerts-notice-base)'
+  container.style.backgroundColor = 'var(--op-color-alerts-notice-base)'
   container.style.height = '100%'
 
   container.className = ['container', size ? `container--${size}` : '', padding ? `container--${padding}-padding` : '']
@@ -12,7 +12,7 @@ export const createContainer = ({ size = '', padding = '' }) => {
     .join(' ')
 
   const innerContents = document.createElement('div')
-  innerContents.style.backgroundColor = 'var(--rm-color-primary-base)'
+  innerContents.style.backgroundColor = 'var(--op-color-primary-base)'
   innerContents.style.height = '100%'
 
   container.appendChild(innerContents)
