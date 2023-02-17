5a42328435b3713c45e719e583ad1f514208911c
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-02-17 12:06:32 -0500

    Update class names

--- src/stories/Recipes/Layout/Layout.js
+++ src/stories/Recipes/Layout/Layout.js
@@ -3,14 +3,14 @@ import { createSidebar } from '../../Sidebar/Sidebar.js'
 export const createLayout = ({ style = 'basic', rightSidebar = false }) => {
   if (style === 'basic') {
     return `
-<div class='app__main' style="height: 80rem;"> <!-- This class should be on body. Height is for demo purposed -->
+<div class='app-body' style="height: 80rem;"> <!-- This class should be on body. Height is for demo purposed -->
   <!-- Flash message here -->
   <!-- Confirm here -->
   <!-- Modal here -->
   <!-- Panel here -->
-  <div class='page__header background-primary-plus-seven'>Header</div>
-  <div class='page__content'>Content</div>
-  <div class='page__footer background-primary-plus-seven'>Footer</div>
+  <div class='app__header background-primary-plus-seven'>Header</div>
+  <div class='app__content'>Content</div>
+  <div class='app__footer background-primary-plus-seven'>Footer</div>
 </div>
 `
   }
@@ -29,20 +29,20 @@ export const createLayout = ({ style = 'basic', rightSidebar = false }) => {
     }
 
     return `
-<div class='app-wrapper' style="height: 40rem;"> <!-- This class should be on body. Height is for demo purposed -->
+<div class='app-with-sidebar' style="height: 40rem;"> <!-- This class should be on body. Height is for demo purposed -->
   <!-- Flash message here -->
   <!-- Confirm here -->
   <!-- Modal here -->
   <!-- Panel here -->
   ${sidebar('default', 'drawer')}
-  <div class='app__main'>
-    <div class='page__header background-primary-plus-seven'>Header</div>
-    <div class='page__content'>
+  <div class='app-body'>
+    <div class='app__header background-primary-plus-seven'>Header</div>
+    <div class='app__content'>
       ${Array.from(Array(16))
         .map((_item) => '<p>Some content</p>')
         .join('\n')}
     </div>
-    <div class='page__footer background-primary-plus-seven'>Footer</div>
+    <div class='app__footer background-primary-plus-seven'>Footer</div>
   </div>
   ${rightSidebar ? sidebar('subtle-primary', 'rail') : ''}
 </div>
