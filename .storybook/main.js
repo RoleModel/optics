ec07fed6c5ceebfe141a3ec35e484d5cdb46a045
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-03-03 15:39:20 -0500

    Add design token package to show tokens in documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/preset-scss',
+    'storybook-design-token',
   ],
   framework: '@storybook/html',
 }
