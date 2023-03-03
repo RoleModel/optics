9f0b795f16e165f8cbcc5126930e2551a8068c46
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-03-03 15:40:14 -0500

    Clean up all token documentation to use new package instead of hard coded values
    Also remove playgrounds for cases that are not needed

--- src/stories/LetterSpacing/LetterSpacing.js
+++
@@ -1,8 +0,0 @@
-export const createLetterSpacing = ({ spacing = 'label' }) => {
-  const element = document.createElement('div')
-
-  element.innerText = 'Spacing'
-  element.style.letterSpacing = `var(--rm-letter-spacing-${spacing})`
-
-  return element
-}
