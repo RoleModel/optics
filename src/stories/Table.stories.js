f714ca7dde34f377b251104918ea1141b1a8538d
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-03-21 13:05:15 -0400

    Update tables to reflect Figma

--- src/stories/Table.stories.js
+++ src/stories/Table.stories.js
@@ -24,6 +24,9 @@ export default {
       control: { type: 'select' },
       options: ['off', 'header', 'footer', 'both'],
     },
+    checkbox: {
+      control: 'boolean',
+    },
     height: {
       control: 'boolean',
       description: 'This is not a class. It just provides a fixed height to demonstrate the sticky classes',
--- src/stories/Table/Table.js
+++ src/stories/Table/Table.js
@@ -5,6 +5,7 @@ export const createTable = ({
   striped = 'off',
   sticky = 'off',
   height = false,
+  checkbox = false,
 }) => {
   const tableContainer = document.createElement('div')
   tableContainer.className = 'table-container'
@@ -32,6 +33,11 @@ export const createTable = ({
   table.innerHTML += `
   <thead>
     <tr>
+      ${
+        checkbox
+          ? "<th><div class='flex items-center gap-sm'><input type='checkbox' /><span>Select</span></div></th>"
+          : ''
+      }
       <th>Movie</th>
       <th>Year Released</th>
       <th>Trilogy</th>
@@ -39,56 +45,67 @@ export const createTable = ({
   </thead>
   <tbody>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode IV - A New Hope</td>
       <td>1977</td>
       <td>Original</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode V - The Empire Strikes Back</td>
       <td>1980</td>
       <td>Original</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode VI - The Return of the Jedi</td>
       <td>1983</td>
       <td>Original</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode I - The Phantom Menace</td>
       <td>1999</td>
       <td>Prequel</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode II - Attack of the Clones</td>
       <td>2002</td>
       <td>Prequel</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode III - The Revenge of the Sith</td>
       <td>2005</td>
       <td>Prequel</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode VII - The Force Awakens</td>
       <td>2015</td>
       <td>Sequel</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Rogue One</td>
       <td>2016</td>
       <td>Anthology</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode VIII - The Last Jedi</td>
       <td>2017</td>
       <td>Sequel</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Solo</td>
       <td>2018</td>
       <td>Anthology</td>
     </tr>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td>Episode IX - The Rise of Skywalker</td>
       <td>2019</td>
       <td>Sequel</td>
@@ -96,6 +113,7 @@ export const createTable = ({
   </tbody>
   <tfoot>
     <tr>
+      ${checkbox ? "<th><input type='checkbox' /></th>" : ''}
       <td colspan="2">Total movies</td>
       <td colspan="1">11</td>
     </tr>
