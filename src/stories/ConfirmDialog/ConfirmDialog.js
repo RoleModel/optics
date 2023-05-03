16cad8a7afbe69454c0e5de3589cad9e26e99b2c
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-05-03 11:05:18 -0400

    Fix extra div in dialog examples

--- src/stories/ConfirmDialog/ConfirmDialog.js
+++ src/stories/ConfirmDialog/ConfirmDialog.js
@@ -1,8 +1,9 @@
 export const createConfirmDialog = ({ title, message, inlineDemo = false }) => {
   const element = document.createElement('div')
 
+  element.classList = `confirm-dialog-wrapper confirm-dialog-wrapper--active ${inlineDemo ? 'confirm-dialog-wrapper--demo' : ''}`
+
   element.innerHTML = `
-<div class="confirm-dialog-wrapper confirm-dialog-wrapper--active ${inlineDemo ? 'confirm-dialog-wrapper--demo' : ''}">
   <div class="confirm-dialog-wrapper__backdrop"></div>
   <div class="confirm-dialog">
     <div class='confirm-dialog__header'>${title}</div>
@@ -14,7 +15,6 @@ export const createConfirmDialog = ({ title, message, inlineDemo = false }) => {
       <button class='btn-delete'>Yes, I'm Sure</button>
     </div>
   </div>
-</div>
 `
 
   return element
