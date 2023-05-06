2e7cfdab3732c796739c79c452ad764c6ffc1687
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-05-06 19:42:11 -0400

    Refactor Card story

---
+++ src/stories/Components/Card/Card.stories.js
@@ -0,0 +1,58 @@
+import { createCard } from './Card.js'
+
+export default {
+  title: 'Components/Card',
+  render: ({ label, ...args }) => {
+    return createCard({ label, ...args })
+  },
+  argTypes: {
+    label: { control: 'text' },
+    padded: { control: 'boolean' },
+    condensed: { control: 'boolean' },
+    header: { control: 'text' },
+    body: { control: 'text' },
+    footer: { control: 'text' },
+    shadow: {
+      control: { type: 'select' },
+      options: ['none', 'x-small', 'small', 'medium', 'large', 'x-large'],
+    },
+  },
+}
+
+export const Plain = {
+  args: {
+    label: 'Plain',
+  },
+}
+
+export const Padded = {
+  args: {
+    padded: true,
+    label: 'Padded',
+  },
+}
+
+export const Full = {
+  args: {
+    header: 'Full',
+    body: 'Body',
+    footer: 'Footer',
+  },
+}
+
+export const Condensed = {
+  args: {
+    condensed: true,
+    header: 'Condensed',
+    body: 'Body',
+    footer: 'Footer',
+  },
+}
+
+export const Shadow = {
+  args: {
+    shadow: 'large',
+    padded: true,
+    label: 'Shadow',
+  },
+}
