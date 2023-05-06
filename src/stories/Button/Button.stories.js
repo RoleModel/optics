86deb759dc59fd43a4d60b6e1d9791c12d1193b8
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-05-06 19:08:14 -0400

    Refactor button story

---
+++ src/stories/Button/Button.stories.js
@@ -0,0 +1,110 @@
+import { createButton } from './Button.js'
+
+export default {
+  title: 'Components/Button',
+  render: ({ label, ...args }) => {
+    return createButton({ label, ...args })
+  },
+  argTypes: {
+    label: { control: 'text' },
+    priority: {
+      control: { type: 'select' },
+      options: ['default', 'primary', 'delete', 'warning'],
+    },
+    noBorder: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+    active: { control: 'boolean' },
+    pill: { control: 'boolean' },
+    icon: { control: 'boolean' },
+    iconWithLabel: { control: 'boolean' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+}
+
+// More on writing stories with args: https://storybook.js.org/docs/html/writing-stories/args
+export const Default = {
+  args: {
+    label: 'Primary',
+    priority: 'default',
+  },
+}
+
+export const Primary = {
+  args: {
+    label: 'Primary',
+    priority: 'primary',
+  },
+}
+
+export const Delete = {
+  args: {
+    label: 'Delete',
+    priority: 'delete',
+  },
+}
+
+export const Warning = {
+  args: {
+    label: 'Warning',
+    priority: 'warning',
+  },
+}
+
+export const Active = {
+  args: {
+    label: 'Active',
+    priority: 'primary',
+    active: true,
+  },
+}
+
+export const NoBorder = {
+  args: {
+    label: 'No Border',
+    priority: 'primary',
+    noBorder: true,
+  },
+}
+
+export const Disabled = {
+  args: {
+    label: 'Disabled',
+    priority: 'primary',
+    disabled: true,
+  },
+}
+
+export const Pill = {
+  args: {
+    label: 'Pill',
+    priority: 'default',
+    pill: true,
+  },
+}
+
+export const Icon = {
+  args: {
+    label: '👍',
+    priority: 'primary',
+    icon: true,
+  },
+}
+
+export const IconWithLabel = {
+  args: {
+    label: 'Label',
+    priority: 'primary',
+    iconWithLabel: true,
+  },
+}
+
+export const Size = {
+  args: {
+    label: 'Small',
+    priority: 'primary',
+    size: 'small',
+  },
+}
