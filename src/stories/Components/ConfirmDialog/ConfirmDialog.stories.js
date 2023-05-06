eea56ded3f802025d84fa47a2091aa814a7ac34d
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-05-06 19:46:58 -0400

    Refactor Confirm Dialog Story

---
+++ src/stories/Components/ConfirmDialog/ConfirmDialog.stories.js
@@ -0,0 +1,27 @@
+import { createConfirmDialog } from './ConfirmDialog.js'
+
+export default {
+  title: 'Components/Confirm Dialog',
+  render: ({ title, ...args }) => {
+    return createConfirmDialog({ title, ...args })
+  },
+  argTypes: {
+    title: { control: 'text' },
+    message: { control: 'text' },
+  },
+}
+
+export const Default = {
+  args: {
+    title: 'Confirm Title',
+    message: 'Are you sure you want to delete this?',
+  },
+}
+
+export const Inline = {
+  args: {
+    title: 'Confirm Title',
+    message: 'Are you sure you want to delete this?',
+    inlineDemo: true,
+  },
+}
