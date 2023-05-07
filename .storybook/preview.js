fee5d71e848d2d8ad928a6e7689160ae7877891f
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-05-06 21:42:56 -0400

    Add theme switcher

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,13 @@
 /** @type { import('@storybook/html').Preview } */
+
 const preview = {
+  decorators: [
+    (Story, context) =>
+      `<div>
+        <script>document.documentElement.dataset.themeMode = '${context.globals.themeMode}'</script>
+        ${Story().outerHTML || Story()}
+      </div>`,
+  ],
   parameters: {
     layout: 'centered',
     controls: {
@@ -24,6 +32,21 @@ const preview = {
 }
 
 export default preview
+
+export const globalTypes = {
+  themeMode: {
+    description: 'Color Theme Mode',
+    defaultValue: 'light',
+    toolbar: {
+      title: 'Theme Mode',
+      icon: 'mirror',
+      items: [
+        { value: 'light', title: 'Light' },
+        { value: 'dark', title: 'Dark' },
+      ],
+    },
+  },
+}
 
 import '!style-loader!css-loader!sass-loader!../src/optics.scss'
 import '!style-loader!css-loader!sass-loader!./documentation.scss'
