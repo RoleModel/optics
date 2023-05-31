6a9fc535ca5eeb3449e2daaefdec7255ce2d798d
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2023-05-31 15:18:39 -0400

    [TR#120] Divider (#172)
    This PR adds a divider component. This can be used to horizontally or vertically divide content.

---
+++ src/stories/Components/Divider/Divider.js
@@ -0,0 +1,14 @@
+export const createDivider = ({ vertical = false, size = 'small', spacing = 'none' }) => {
+  const element = document.createElement('div')
+  element.className = vertical ? 'divider-vertical' : 'divider'
+
+  if (size !== 'small') {
+    element.classList.add(`divider--${size}`)
+  }
+
+  if (spacing !== 'none') {
+    element.classList.add(`divider--spacing-${spacing}`)
+  }
+
+  return element
+}
