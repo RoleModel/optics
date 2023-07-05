2e75563445ea770689ad27059caf0018d22d5c83
Dallas Peters <dallas.peters@rolemodelsoftware.com>
2023-07-05 10:29:56 -0500

    adds accessibility markup and styles

--- src/stories/Components/Spinner/Spinner.js
+++ src/stories/Components/Spinner/Spinner.js
@@ -4,6 +4,8 @@ export const createSpinner = ({
   const element = document.createElement('div')
 
   element.className = `spinner spinner--${size}`
+  element.role = `status`
+  element.innerHTML = `<span class="assistive-text">Loading</span>`
 
   return element
 }
