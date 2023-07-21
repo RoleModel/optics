c3a016da998cbab6346a5bab16435319c9041c41
Dallas Peters <dallas@dallaspeters.co>
2023-07-21 15:15:28 -0500

    [TR#10] Add tags component (#186)
    This PR adds a new component. The Tag component! While similar to Badge, it serves a distinct semantic purpose that being interaction and input. This also adds some new tokens to fill out the full scale of input focus states.

---
+++ src/stories/Components/Tag/Tag.js
@@ -0,0 +1,22 @@
+import { createButton } from '../Button/Button.js'
+
+export const createTag = ({ label, type = 'readonly', iconLeft = false, iconRight = false }) => {
+  const tag = document.createElement('div')
+
+  const text = document.createElement('span')
+  text.innerText = label
+  text.className = 'tag__label'
+  tag.append(text)
+
+  if (iconLeft) {
+    tag.prepend(createButton({ priority: 'default', noBorder: true, icon: 'close', pill: true }))
+  }
+
+  if (iconRight) {
+    tag.append(createButton({ priority: 'default', noBorder: true, icon: 'close', pill: true }))
+  }
+
+  tag.className = [`tag-${type}`].filter(Boolean).join(' ')
+
+  return tag
+}
