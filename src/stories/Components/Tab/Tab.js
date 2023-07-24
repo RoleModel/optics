e764e30a7f32bec75ded4d792ecee9204b540e41
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-07-24 13:42:48 -0400

    Fix focus states

--- src/stories/Components/Tab/Tab.js
+++ src/stories/Components/Tab/Tab.js
@@ -3,15 +3,29 @@ export const createTab = ({ size = 'large', activeTab = 'USA', disabledTab = 'Ca
   element.className = 'tab-group'
 
   const createTabItem = (item) => {
-    return `<a href='/?path=/docs/components-tab--docs' class='tab${size === 'small' ? ' tab--small' : ''}${
-      activeTab === item ? ' tab--active' : ''
-    }${disabledTab === item ? ' tab--disabled' : ''}'>${item}</a>`
+    const link = document.createElement('a')
+    link.href = '/?path=/docs/components-tab--docs'
+
+    link.className = [
+      'tab',
+      size === 'small' ? 'tab--small' : '',
+      activeTab === item ? ' tab--active' : '',
+      disabledTab === item ? ' tab--disabled' : ''
+    ].filter(Boolean).join(' ')
+
+    link.innerText = item
+
+    if (disabledTab === item) {
+      link.setAttribute('tabindex', -1)
+    }
+
+    return link
   }
 
   const tabs = ['USA', 'Mexico', 'Canada', 'England', 'Germany', 'Spain', 'Austria', 'Australia']
 
   element.innerHTML += `
-  ${tabs.map((item) => createTabItem(item)).join('\n  ')}
+  ${tabs.map((item) => createTabItem(item).outerHTML).join('\n  ')}
 `
 
   return element
