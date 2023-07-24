d854bbc01a3139cf3b7ca25c593b8aec12c5df91
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2023-07-24 13:57:49 -0400

    [TR#13] Add tab component (#195)
    This PR adds a new component. Tabs!

---
+++ src/stories/Components/Tab/Tab.js
@@ -0,0 +1,34 @@
+export const createTab = ({ size = 'large', activeTab = 'USA', disabledTab = 'Canada' }) => {
+  const element = document.createElement('div')
+  element.className = 'tab-group'
+
+  const createTabItem = (item) => {
+    const link = document.createElement('a')
+    link.href = '/?path=/docs/components-tab--docs'
+
+    link.className = [
+      'tab',
+      size === 'small' ? 'tab--small' : '',
+      activeTab === item ? ' tab--active' : '',
+      disabledTab === item ? ' tab--disabled' : '',
+    ]
+      .filter(Boolean)
+      .join(' ')
+
+    link.innerText = item
+
+    if (disabledTab === item) {
+      link.setAttribute('tabindex', -1)
+    }
+
+    return link
+  }
+
+  const tabs = ['USA', 'Mexico', 'Canada', 'England', 'Germany', 'Spain', 'Austria', 'Australia']
+
+  element.innerHTML += `
+  ${tabs.map((item) => createTabItem(item).outerHTML).join('\n  ')}
+`
+
+  return element
+}
