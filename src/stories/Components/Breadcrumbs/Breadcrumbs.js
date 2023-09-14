d318eb3da6f2f62e7f1c74ee828f52c6b4f5d235
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-09-14 14:29:20 -0400

    Add breadcrumb component

---
+++ src/stories/Components/Breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,50 @@
+import { createIcon } from '../Icon/Icon'
+
+export const createBreadcrumbs = ({ size = '', separator = 'arrow_right', gap = 'default' }) => {
+  const element = document.createElement('nav')
+
+  element.className = [
+    'breadcrumbs',
+    size === '' ? '' : `breadcrumbs--${size}`,
+    gap === 'default' ? '' : gap,
+  ].filter(Boolean).join(' ')
+
+  const links = [
+    { type: 'link', text: 'USA' },
+    { type: 'separator', text: separator },
+    { type: 'link', text: 'Mexico' },
+    { type: 'separator', text: separator },
+    { type: 'link', text: 'Canada' },
+    { type: 'separator', text: separator },
+    { type: 'text', text: 'England' },
+  ]
+
+  const elementFor = (type) => {
+    return {
+      link: 'a',
+      text: 'span',
+      separator: 'div',
+    }[type]
+  }
+
+  const createLink = (linkItem) => {
+    const element = document.createElement(elementFor(linkItem.type))
+
+    element.className = `breadcrumbs__${linkItem.type}`
+    element.innerText = linkItem.text
+
+    if (linkItem.type === 'link') {
+      element.href = '#'
+    } else if (linkItem.type === 'separator') {
+      element.innerHTML = '\n    ' + createIcon({ name: linkItem.text }).outerHTML + '\n  '
+    }
+
+    return element
+  }
+
+  element.innerHTML += `
+  ${links.map((item, index) => createLink(item, index).outerHTML).join('\n  ')}
+`
+
+  return element
+}
