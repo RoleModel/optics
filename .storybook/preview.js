05f0c7e5abfc19ac2fd678a88db60f1278bbe229
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2024-05-21 21:46:01 -0400

    Add category sorting back

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,6 +7,31 @@ const preview = {
         date: /Date$/i,
       },
     },
+    options: {
+      storySort: {
+        order: [
+          'Overview',
+          [
+            'Introduction',
+            'Base Reset',
+            'Organization',
+            'Selective Imports',
+            'Tokens',
+            'Themes',
+            'Scale Overriding',
+            'Addons',
+          ],
+          'Tokens',
+          'Utilities',
+          'Navigation Components',
+          'Content Components',
+          'Popup Content Components',
+          'Visual Components',
+          'Components',
+          'Recipes',
+        ],
+      }
+    }
   },
 }
 
