f66f3d1d6cae97ec5a76a256553e435f643f70ca
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2024-05-23 19:45:48 -0400

    Remove already converted stories

--- old/src/stories/Tokens/Animation/Animation.js
+++
@@ -1,9 +0,0 @@
-export const createAnimation = ({ speed = 'input (120ms)' }) => {
-  const element = document.createElement('div')
-
-  element.innerText = `${speed} Hover Me!`
-
-  element.className = `transition-demo transition-demo--${speed}`
-
-  return element
-}
