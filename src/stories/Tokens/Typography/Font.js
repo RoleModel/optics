d426c85953647cbab5026c366b6c54e76b710373
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2024-07-31 09:54:57 -0400

    Add alt font family

--- src/stories/Tokens/Typography/Font.js
+++ src/stories/Tokens/Typography/Font.js
@@ -2,8 +2,12 @@ export const createFontFamily = ({ family = 'Noto Sans' }) => {
   const element = document.createElement('p')
   element.innerText = `Font Family Demo using: ${family}`
 
-  if (family === 'Tilt Neon') {
+  if (family === 'Noto Serif') {
     element.className = 'font-family-demo--alternative'
+  }
+
+  if (family === 'Tilt Neon') {
+    element.className = 'font-family-demo--optional'
   }
 
   return element
