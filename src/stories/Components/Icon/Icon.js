d2774472154dbb42c910313244bbfc9a273d4f25
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2025-02-12 15:46:09 -0500

    [OP-28] Custom Icon Support (#273)
    This PR adds support for new icon packs. It adds Phosphor, Tabler, Feather, and Lucide. It also adds documentation on how to utilize them

--- src/stories/Components/Icon/Icon.js
+++ src/stories/Components/Icon/Icon.js
@@ -1,17 +1,126 @@
-export const createIcon = ({ name, filled = false, size = 'medium', weight = 'normal', emphasis = 'normal' }) => {
-  const icon = document.createElement('span')
-  icon.innerText = name
+class IconPackType {
+  constructor(packName, options = {}) {
+    this.packName = packName
+    this.options = options
+  }
 
-  icon.className = [
-    'icon',
-    'material-symbols-outlined',
-    filled ? 'icon--filled' : '',
-    size === 'medium' ? '' : `icon--${size}`,
-    weight === 'normal' ? '' : `icon--weight-${weight}`,
-    emphasis === 'normal' ? '' : `icon--${emphasis}-emphasis`,
-  ]
-    .filter(Boolean)
-    .join(' ')
+  static fromNameAndOptions(packName, options) {
+    const typeMapping = {
+      'Material Symbols Outlined': MaterialIconPack,
+      Phosphor: PhosphorIconPack,
+      Tabler: TablerIconPack,
+      Feather: FeatherIconPack,
+      Lucide: LucideIconPack,
+    }
 
-  return icon
+    return new typeMapping[packName](packName, options)
+  }
+
+  createIcon() {
+    const icon = document.createElement(this.iconElement())
+    icon.innerText = this.innerText()
+    icon.className = this.iconClasses().filter(Boolean).join(' ')
+    return icon
+  }
+
+  iconElement() {
+    // Subclasses should override this method
+    return 'div'
+  }
+
+  innerText() {
+    // Subclasses may wish to override this method
+    return ''
+  }
+
+  iconClasses() {
+    return ['icon', this.options.size === 'medium' ? '' : `icon--${this.options.size}`]
+  }
+}
+
+class MaterialIconPack extends IconPackType {
+  iconElement() {
+    return 'span'
+  }
+
+  innerText() {
+    return this.options.iconName
+  }
+
+  iconClasses() {
+    return [
+      'material-symbols-outlined',
+      this.options.filled ? 'icon--filled' : '',
+      this.options.weight === 'normal' ? '' : `icon--weight-${this.options.weight}`,
+      this.options.emphasis === 'normal' ? '' : `icon--${this.options.emphasis}-emphasis`,
+    ].concat(super.iconClasses())
+  }
+}
+
+class PhosphorIconPack extends IconPackType {
+  iconElement() {
+    return 'i'
+  }
+
+  iconClasses() {
+    return [
+      this.options.duotone ? 'ph-duotone' : 'ph',
+      `ph-${this.options.iconName}`,
+      this.options.filled ? 'icon--filled' : '',
+      this.options.weight === 'normal' ? '' : `icon--weight-${this.options.weight}`,
+    ].concat(super.iconClasses())
+  }
+}
+
+class TablerIconPack extends IconPackType {
+  iconElement() {
+    return 'i'
+  }
+
+  iconClasses() {
+    return ['ti', this.options.filled ? `ti-${this.options.iconName}-filled` : `ti-${this.options.iconName}`].concat(
+      super.iconClasses()
+    )
+  }
+}
+
+class FeatherIconPack extends IconPackType {
+  iconElement() {
+    return 'i'
+  }
+
+  iconClasses() {
+    return ['fi', `fi-${this.options.iconName}`].concat(super.iconClasses())
+  }
+}
+
+class LucideIconPack extends IconPackType {
+  iconElement() {
+    return 'i'
+  }
+
+  iconClasses() {
+    return ['li', `li-${this.options.iconName}`].concat(super.iconClasses())
+  }
+}
+
+export const createIcon = ({
+  iconPack = 'Material Symbols Outlined',
+  name,
+  filled = false,
+  size = 'medium',
+  weight = 'normal',
+  emphasis = 'normal',
+  duotone = false,
+}) => {
+  const iconPackType = IconPackType.fromNameAndOptions(iconPack, {
+    iconName: name,
+    filled,
+    duotone,
+    weight,
+    size,
+    emphasis,
+  })
+
+  return iconPackType.createIcon()
 }
