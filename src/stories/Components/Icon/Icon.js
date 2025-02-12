20a605356c520092a61e0bc9acd9d9760cbbcd41
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-02-12 10:19:52 -0500

    Refactor icon html generation to utilize a factory method with objects

--- src/stories/Components/Icon/Icon.js
+++ src/stories/Components/Icon/Icon.js
@@ -1,14 +1,72 @@
-const createPhosphorElement = (name, duotone) => {
-  const icon = document.createElement('i')
-  icon.className = [duotone ? 'ph-duotone' : 'ph', `ph-${name} `].filter(Boolean).join(' ')
-  return icon
+class IconPackType {
+  constructor(packName, options = {}) {
+    this.packName = packName
+    this.options = options
+  }
+
+  static fromNameAndOptions(packName, options) {
+    const typeMapping = {
+      'Material Symbols Outlined': MaterialIconPack,
+      Phosphor: PhosphorIconPack,
+    }
+
+    return new typeMapping[packName](packName, options)
+  }
+
+  createIcon() {
+    const icon = document.createElement(this.iconElement())
+    icon.innerText = this.innerText()
+    icon.className = this.iconClasses().filter(Boolean).join(' ')
+    return icon
+  }
+
+  iconElement() {
+    // Subclasses should override this method
+    return 'div'
+  }
+
+  innerText() {
+    // Subclasses may wish to override this method
+    return ''
+  }
+
+  iconClasses() {
+    return ['icon', this.options.size === 'medium' ? '' : `icon--${this.options.size}`]
+  }
 }
 
-const createMaterialElement = (name) => {
-  const icon = document.createElement('span')
-  icon.innerText = name
-  icon.className = 'material-symbols-outlined '
-  return icon
+class MaterialIconPack extends IconPackType {
+  iconElement() {
+    return 'span'
+  }
+
+  innerText() {
+    return this.options.iconName
+  }
+
+  iconClasses() {
+    return [
+      'material-symbols-outlined',
+      this.options.filled ? 'icon--filled' : '',
+      this.options.weight === 'normal' ? '' : `icon--weight-${this.options.weight}`,
+      this.options.emphasis === 'normal' ? '' : `icon--${this.options.emphasis}-emphasis`,
+    ].concat(super.iconClasses())
+  }
+}
+
+class PhosphorIconPack extends IconPackType {
+  iconElement() {
+    return 'i'
+  }
+
+  iconClasses() {
+    return [
+      this.options.duotone ? 'ph-duotone' : 'ph',
+      `ph-${this.options.iconName}`,
+      this.options.filled ? 'icon--filled' : '',
+      this.options.weight === 'normal' ? '' : `icon--weight-${this.options.weight}`,
+    ].concat(super.iconClasses())
+  }
 }
 
 export const createIcon = ({
@@ -20,25 +78,14 @@ export const createIcon = ({
   emphasis = 'normal',
   duotone = false,
 }) => {
-  let icon
+  const iconPackType = IconPackType.fromNameAndOptions(iconPack, {
+    iconName: name,
+    filled,
+    duotone,
+    weight,
+    size,
+    emphasis,
+  })
 
-  if (iconPack === 'Phosphor') {
-    icon = createPhosphorElement(name, duotone)
-  }
-
-  if (iconPack === 'Material Symbols Outlined') {
-    icon = createMaterialElement(name)
-  }
-
-  icon.className += [
-    'icon',
-    filled ? 'icon--filled' : '',
-    size === 'medium' ? '' : `icon--${size}`,
-    weight === 'normal' ? '' : `icon--weight-${weight}`,
-    emphasis === 'normal' ? '' : `icon--${emphasis}-emphasis`,
-  ]
-    .filter(Boolean)
-    .join(' ')
-
-  return icon
+  return iconPackType.createIcon()
 }
