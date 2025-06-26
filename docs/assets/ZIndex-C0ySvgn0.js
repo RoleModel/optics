3f9fcaae8f789d66143b417e74be28f20e20de1d
Jeremy-Walton <Jeremy-Walton@users.noreply.github.com>
2025-06-26 01:05:34 +0000

    Deploying to gh-pages from  @ 61226a19d49e96555263c066597fdb99cd3fc93e 🚀

--- docs/assets/ZIndex-C0ySvgn0.js
+++
@@ -1,9 +0,0 @@
-import{j as e,M as i}from"./index-DyF4keL0.js";import{useMDXComponents as s}from"./index-CiOQkYpe.js";import{N as r}from"./index-CHtO0fOR.js";import{c as a}from"./sourceCodeLink-Chh3FteF.js";import"./iframe-CgDlpB7V.js";import"./index-BiL3ubqk.js";import"./index-DrFu-skq.js";function o(t){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Tokens/Z-Index"}),`
-`,e.jsx(n.h1,{id:"z-index",children:"Z-Index"}),`
-`,e.jsx("div",{dangerouslySetInnerHTML:{__html:a({link:"core/tokens/base_tokens.css"}).outerHTML}}),`
-`,e.jsx(n.p,{children:"Z-Index tokens can be used to define the page z-index of an element."}),`
-`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
-`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`z-index: var(--op-z-index-header);
-`})}),`
-`,e.jsx(n.h2,{id:"available-tokens-and-their-definitions",children:"Available tokens and their definitions"}),`
-`,e.jsx(r,{categoryName:"Z-Index",viewType:"card"})]})}function j(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{j as default};
