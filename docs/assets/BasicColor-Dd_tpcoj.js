1e233324c91904a8feab315392e1837afbe51b7b
Jeremy-Walton <Jeremy-Walton@users.noreply.github.com>
2025-06-27 14:12:33 +0000

    Deploying to gh-pages from  @ 069d746dd3d1feb343b936b148171f0b5cc3a2a1 🚀

---
+++ docs/assets/BasicColor-Dd_tpcoj.js
@@ -0,0 +1,16 @@
+import{j as o,M as t}from"./index-CCw2sMFK.js";import{useMDXComponents as s}from"./index-Cp82c81x.js";import{c}from"./sourceCodeLink-Chh3FteF.js";import{N as a}from"./index-Dh2ko8Tu.js";import"./iframe-Bg2t51us.js";import"./index-BiL3ubqk.js";import"./index-DrFu-skq.js";function r(n){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(t,{title:"Tokens/Color/Basic Color"}),`
+`,o.jsx(e.h1,{id:"basic-color",children:"Basic Color"}),`
+`,o.jsx("div",{dangerouslySetInnerHTML:{__html:c({link:"core/tokens/base_tokens.css"}).outerHTML}}),`
+`,o.jsx(e.p,{children:"Basic color tokens can be used to define exact colors."}),`
+`,o.jsx(e.p,{children:"The background colors use the color scale and will adjust in dark mode."}),`
+`,o.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(e.p,{children:"These tokens can be applied to anything that supports color."}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-css",children:`body {
+  background-color: var(--op-color-background);
+  color: var(--op-color-on-background);
+}
+/* or */
+color: var(--op-color-white);
+`})}),`
+`,o.jsx(e.h2,{id:"available-tokens-and-their-definitions",children:"Available tokens and their definitions"}),`
+`,o.jsx(a,{categoryName:"Basic Colors",viewType:"card"})]})}function x(n={}){const{wrapper:e}={...s(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(r,{...n})}):r(n)}export{x as default};
---
+++ docs/assets/FileOrganization-BpZGckUj.js
@@ -0,0 +1,80 @@
+import{j as e,M as r}from"./index-CCw2sMFK.js";import{useMDXComponents as s}from"./index-Cp82c81x.js";import"./iframe-Bg2t51us.js";import"./index-BiL3ubqk.js";import"./index-DrFu-skq.js";function o(i){const n={code:"code",h1:"h1",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Overview/File Organization"}),`
+`,e.jsx(n.h1,{id:"file-organization",children:"File Organization"}),`
+`,e.jsx(n.p,{children:"As you use Optics, you will likely want to organize your Optics configuration into multiple files."}),`
+`,e.jsx(n.h2,{id:"folder-structure",children:"Folder Structure"}),`
+`,e.jsxs(n.p,{children:["The recommended folder structure is to have a ",e.jsx(n.code,{children:"main or application"})," file that is the root file. This file should import all of the other files in the folder."]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"/stylesheets"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"application.css"})}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"/core"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"/theme"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"{name}-theme-core.css"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"{name}-theme-light.css"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"{name}-theme-dark.css"})}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"base.css"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"layout.css"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"utilities.css"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"/vendors"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"{vendor-name}-overrides.css"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"/components"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"/optics-overrides"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"{component-name}.css"})}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"{component-name}.css"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"/general"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"{general-name}.css"})}),`
+`]}),`
+`]}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"import-structure",children:"Import Structure"}),`
+`,e.jsx(n.p,{children:"Using the above folder structure, the application file should import all of the other files. Comments can be used to describe the purpose of each section of files."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`/* Optics */
+@import '@rolemodel/optics';
+
+/* Vendors */
+@import 'something-from-node-modules/something';
+
+/* Font Customization */
+@import 'core/fonts/text_fonts';
+@import 'core/fonts/icon_fonts';
+
+/* Theme Customization */
+@import 'core/theme/{name}-theme-core';
+@import 'core/theme/{name}-theme-light';
+@import 'core/theme/{name}-theme-dark';
+
+/* Core Customization */
+@import 'core/base';
+@import 'core/layout';
+@import 'core/utilities';
+
+/* Vendor Customization */
+@import 'vendors/{vendor-name}-overrides';
+
+/* Optics Component Customization */
+@import 'components/optics-overrides/{component-name}';
+
+/* Component */
+@import 'components/{component-name}';
+
+/* General Styles */
+@import 'general/{general-name}';
+`})})]})}function m(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(o,{...i})}):o(i)}export{m as default};
