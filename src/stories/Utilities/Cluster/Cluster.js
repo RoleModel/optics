520d415f2ab66fb5e069f620d3c53e44c0046a61
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-12-08 21:25:53 -0500

    Add cluster utility

---
+++ src/stories/Utilities/Cluster/Cluster.js
@@ -0,0 +1,14 @@
+import { createChildren } from '../../helpers/utils'
+
+export const createCluster = ({ cluster = true, alignItems = '', gap = '' }) => {
+  const wrapper = document.createElement('div')
+  wrapper.style.height = '15rem'
+
+  wrapper.className = [cluster ? 'cluster' : '', alignItems ? `items-${alignItems}` : '', gap ? `gap-${gap}` : '']
+    .filter(Boolean)
+    .join(' ')
+
+  createChildren(wrapper, 35)
+
+  return wrapper
+}
