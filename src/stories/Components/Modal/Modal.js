05a41fa0c58ed1110a9fe911a957722eddcf6e1c
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-12-30 15:41:41 -0500

    Re-implement modal for the dialog using modern approaches. Update documentation to reflect

--- src/stories/Components/Modal/Modal.js
+++ src/stories/Components/Modal/Modal.js
@@ -1,4 +1,8 @@
-export const createModal = ({ header, body, footer = '', inlineDemo = false }) => {
+export const createModal = ({ header, body, footer = '', inlineDemo = false, dialog = false }) => {
+  if (dialog) {
+    return createDialogModal({ header, body, footer })
+  }
+
   const element = document.createElement('div')
 
   element.classList = `modal-wrapper modal-wrapper--active ${inlineDemo ? 'modal-wrapper--demo' : ''}`
@@ -30,4 +34,40 @@ export const createModal = ({ header, body, footer = '', inlineDemo = false }) =
 `
 
   return element
+}
+
+export const createDialogModal = ({ header, body, footer = '' }) => {
+  const example = document.createElement('div')
+
+  const footerContents =
+    footer !== ''
+      ? footer
+      : `
+        <form method="dialog">
+          <button class="btn">Cancel</button>
+        </form>
+        <button class='btn btn--primary'>Save</button>
+  `
+
+  example.innerHTML = `
+    <button class="btn" commandfor="my-dialog" command="show-modal">Show Dialog</button>
+    <dialog id="my-dialog" class="modal" closedby="any">
+      <div class='modal__header'>
+        ${header}
+        <form method="dialog">
+          <button class="btn btn--no-border btn--icon btn--pill">
+            <span class='material-symbols-outlined icon--x-large'>close</span>
+          </button>
+        </form>
+      </div>
+      <div class='modal__body'>
+        ${body}
+      </div>
+      <div class='modal__footer'>
+        ${footerContents}
+      </div>
+    </dialog>
+  `
+
+  return example
 }
